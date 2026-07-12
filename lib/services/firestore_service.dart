ad5786c76d6490039ede12e8f33370eac0721ff6
ardhikaxx <ardhikayanuar58@gmail.com>
2026-07-12 09:19:46 +0700

    feat: tambahkan layanan penyimpanan data progres ke firestore

---
+++ lib/services/firestore_service.dart
@@ -0,0 +1,51 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../models/user_progress_model.dart';
+
+class FirestoreService {
+  static final FirestoreService instance = FirestoreService._();
+  FirestoreService._();
+
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  DocumentReference _userDoc(String uid) =>
+      _firestore.collection('users').doc(uid);
+
+  Future<UserProgress?> getUserProgress(String uid) async {
+    final doc = await _userDoc(uid).get();
+    if (!doc.exists) return null;
+    return UserProgress.fromMap(doc.data() as Map<String, dynamic>);
+  }
+
+  Stream<UserProgress?> getUserProgressStream(String uid) {
+    return _userDoc(uid).snapshots().map((doc) {
+      if (!doc.exists) return null;
+      return UserProgress.fromMap(doc.data() as Map<String, dynamic>);
+    });
+  }
+
+  Future<void> saveTopikProgress(
+      String uid, int kelas, String topikId, TopicProgress progress) async {
+    final key = '$kelas-$topikId';
+    await _userDoc(uid).update({
+      'topikProgress.$key': progress.toMap(),
+    });
+  }
+
+  Future<void> updateKelasAktif(String uid, int kelas) async {
+    await _userDoc(uid).update({'kelasAktif': kelas});
+  }
+
+  Future<void> initUserProgress(String uid, String nama, String email) async {
+    final doc = await _userDoc(uid).get();
+    if (!doc.exists) {
+      await _userDoc(uid).set({
+        'uid': uid,
+        'nama': nama,
+        'email': email,
+        'kelasAktif': 1,
+        'topikProgress': {},
+        'createdAt': FieldValue.serverTimestamp(),
+      });
+    }
+  }
+}
